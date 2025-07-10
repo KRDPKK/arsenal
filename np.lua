4f46c69433254e4b77c62b48f525bd141e135f0e
KRDPKK <Benjamin.Molla@yahoo.com>
2025-07-10 12:32:00 +0200

    Create np.lua

---
+++ np.lua
@@ -0,0 +1,12 @@
+while wait() do
+    game.Players.LocalPlayer.PlayerGui.GUI_Scorecard.Scorecard.Scrolling.Visible = false
+    game.Workspace.KillFeed["1"].Killer.Value = "discord.gg/qkWVWph6ck"
+    game.Workspace.KillFeed["2"].Killer.Value = "discord.gg/qkWVWph6ck"
+    game.Workspace.KillFeed["3"].Killer.Value = "discord.gg/qkWVWph6ck"
+    game.Workspace.KillFeed["4"].Killer.Value = "discord.gg/qkWVWph6ck"
+    game.Workspace.KillFeed["5"].Killer.Value = "discord.gg/qkWVWph6ck"
+    game.Workspace.KillFeed["6"].Killer.Value = "discord.gg/qkWVWph6ck"
+    game.Players.LocalPlayer.PlayerGui.GUI.Winner.Visible = false
+    game.Players.LocalPlayer.PlayerGui.Menew_Main.Container.PlrName.Text = "discord.gg/qkWVWph6ck"
+    game.Players.LocalPlayer.PlayerGui.GUI_Scorecard.Scorecard.PlayerCard.Username.Text = "qqwizzixxxx >:)"
+end
